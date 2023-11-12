4c5a8d159dcbffc4b21c48a42207a3fb7fb713eb
RicoLittawa <ricolittawa030620@gmail.com>
2023-11-12 19:17:46 +0800

    edited server origin

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const uri = "mongodb+srv://ricolittawa030620:8LpeM1AS2SyRTYUl@cdrrmo.bfvmf8d.mon
 
 const app = express();
 app.use(cors({
-  origin:["hhttps://response-report-cdrrmo.vercel.app"],
+  origin:["https://frontend-ten-xi-26.vercel.app/"],
   methods:["POST","GET"],
   credentials:true
 }));
